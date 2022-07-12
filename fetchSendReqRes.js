2d91f626fee7a4fe200b2c45a17b285b54ca819f
Jyuyo <43424853+Jyuyo@users.noreply.github.com>
2022-07-12 10:21:15 -0500

    Update fetchSendReqRes.js

--- fetchSendReqRes.js
+++ fetchSendReqRes.js
@@ -3,14 +3,14 @@ const { default: fetch } = require("node-fetch");
 
 const fetchReqResDoc = (endpoint, reqRes) => {
 
-    const URL = `${process.env.APIDOC_URL}${endpoint}`
+    const url = `${process.env.APIDOC_URL}${endpoint}`
 
     const requestOptions = {
         method: 'PUT',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(reqRes)
     };
-    fetch(URL, requestOptions)
+    fetch(url, requestOptions)
         .catch(e => console.log(e))
     
 }
